01969955acdf80b93283179cd879034756887748
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-24 11:23:18 -0800

    write branching

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -9,6 +9,21 @@ namespace ToDoList
         public static void Main()
         {
             Console.WriteLine("Welcome to the To Do List");
+            Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+            string reply = Console.ReadLine();
+            if (reply != "Add" && reply != "View" && string.IsNullOrEmpty(reply))
+            {
+                Console.WriteLine("Error! Please respond to the previous question with \"Add\" or \"View\"");
+            }
+            else if(reply == "Add")
+            {
+                // Add branch
+            }
+            else
+            {
+                // View branch
+            }
+
         }
     }
 }
