a5315baf030cf7a09aedba3a08871ec8f6469c22
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-24 13:37:05 -0800

    add functionality for to add item in a list and display list of items

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -14,14 +14,24 @@ namespace ToDoList
             if (reply != "Add" && reply != "View" && string.IsNullOrEmpty(reply))
             {
                 Console.WriteLine("Error! Please respond to the previous question with \"Add\" or \"View\"");
+                Main();
             }
             else if(reply == "Add")
             {
-                // Add branch
+             Console.WriteLine("Please enter the description for the new item.");
+        reply = Console.ReadLine();
+        Item newItem = new Item(reply);
+        Main();
+
             }
             else
             {
-                // View branch
+                 List<Item> test = Item.GetAll();
+        foreach(Item todo in test)
+        {
+          Console.WriteLine(todo.Description);
+        }
+        Main();
             }
 
         }
