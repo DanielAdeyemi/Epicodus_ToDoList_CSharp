9b3e169c0d85a93e0f3d3ec815d8954e3e6b662d
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-23 21:43:08 -0800

    add static List and create static Method

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,11 +1,20 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
     public string Description {get; set; }
+    private static List<Item> _instances = new List<Item> {};
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this);
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
     }
   }
 }
