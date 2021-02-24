05780c5b89b2194e69967132d96da5296e209cbc
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-23 21:09:48 -0800

    update class

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,6 +2,10 @@ namespace ToDoList.Models
 {
   public class Item
   {
-      
+    public string Description {get; }
+    public Item(string description)
+    {
+      Description = description;
+    }
   }
 }
