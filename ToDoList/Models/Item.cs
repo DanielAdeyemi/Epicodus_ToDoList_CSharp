d622a83fc626e499d6073f95b3c23dbe76ab286e
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-23 21:17:55 -0800

    set setter to pass the test

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,7 +2,7 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description {get; }
+    public string Description {get; set; }
     public Item(string description)
     {
       Description = description;
