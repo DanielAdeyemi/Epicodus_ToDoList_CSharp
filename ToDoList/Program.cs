b81afa124bad3dcff20ac961a2ca3ebb8e9072e9
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-02 20:49:13 -0800

    setup project for web

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -5,49 +5,48 @@ using System.Linq;
 
 namespace ToDoList
 {
-    class Program
+  class Program
+  {
+    public static void Main()
     {
-        public static void Main()
-        {
-            Console.WriteLine("Welcome to the To Do List");
-            Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-            string reply = Console.ReadLine();
-            if (reply != "Add" && reply != "View" && string.IsNullOrEmpty(reply))
-            {
-                Console.WriteLine("Error! Please respond to the previous question with \"Add\" or \"View\"");
-                Main();
-            }
-            else if(reply == "Add")
-            {
-                Console.WriteLine("Please enter the description for the new item.");
-                reply = Console.ReadLine();
-                Item newItem = new Item(reply);
-                Main();
-                
-
-            }
-            else
-            {
-                if(Item.GetAll().Any())
-        {
-        Item.GetAll().ForEach(x => {
-          Console.WriteLine($"{x.counter}. {x.Description}");
-        });
+      Console.WriteLine("Welcome to the To Do List");
+      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+      string reply = Console.ReadLine();
+      if (reply != "Add" && reply != "View" && string.IsNullOrEmpty(reply))
+      {
+        Console.WriteLine("Error! Please respond to the previous question with \"Add\" or \"View\"");
         Main();
+      }
+      else if (reply == "Add")
+      {
+        Console.WriteLine("Please enter the description for the new item.");
+        reply = Console.ReadLine();
+        Item newItem = new Item(reply);
+        Main();
+      }
+      else
+      {
+        if (Item.GetAll().Any())
+        {
+          Item.GetAll().ForEach(x =>
+          {
+            Console.WriteLine($"{x.counter}. {x.Description}");
+          });
+          Main();
         }
-        else 
+        else
         {
           Console.WriteLine("You don't have anything to do - enjoy your life!");
         }
-                
-            //     List<Item> test = Item.GetAll();
-            //     foreach(Item todo in test)
-            //     {
-            //         Console.WriteLine(todo.Description);
-            //     }
-            // Main();
-            }
 
-        }
+        //     List<Item> test = Item.GetAll();
+        //     foreach(Item todo in test)
+        //     {
+        //         Console.WriteLine(todo.Description);
+        //     }
+        // Main();
+      }
+
     }
+  }
 }
