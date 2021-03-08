98fe0720b13550ea15a5f36bd8d1d552cc020a7c
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-07 21:35:35 -0800

    finish find method

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -87,10 +87,31 @@ namespace ToDoList.Models
         conn.Dispose();
       }
     }
-    public static Item Find(int searchId)
+    public static Item Find(int id)
     {
-      Item ph = new Item("ph");
-      return ph;
+       MySqlConnection conn = DB.Connection();
+       conn.Open();
+       var cmd = conn.CreateCommand() as MySqlCommand;
+       cmd.CommandText = @"SELECT * FROM items WHERE id = @thisId;";
+       MySqlParameter thisId = new MySqlParameter();
+       thisId.ParameterName = "@thisId";
+       thisId.Value = id;
+       cmd.Parameters.Add(thisId);
+       var rdr = cmd.ExecuteReader() as MySqlDataReader;
+       int itemId = 0;
+       string itemDescription = "";
+       while (rdr.Read())
+       {
+          itemId = rdr.GetInt32(0);
+          itemDescription = rdr.GetString(1);
+       }
+       Item foundItem = new Item(itemDescription, itemId);
+       conn.Close();
+       if (conn != null)
+       {
+         conn.Dispose();
+       }
+       return foundItem;
     }
   }
 }
