be1e3a7dc8efae54e8c482eac7b1ddb7c5a95150
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-07 21:04:52 -0800

    create save method

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -70,7 +70,21 @@ namespace ToDoList.Models
 
     public void Save()
     {
-      
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@ItemDescription";
+      description.Value = this.Description;
+      cmd.Parameters.Add(description);
+      cmd.ExecuteNonQuery();
+      // Id = cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
     public static Item Find(int searchId)
     {
