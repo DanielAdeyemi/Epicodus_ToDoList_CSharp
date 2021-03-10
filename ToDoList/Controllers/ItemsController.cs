135b40a40cc6760f0c0374310d32ea37d3b18faa
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-09 20:32:48 -0800

    create delete route

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -53,6 +53,20 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item=> item.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Item.FirstOrDefault(item => item.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
     // [HttpGet("/categories/{categoryId}/items/new")]
     // public ActionResult New(int categoryId)
