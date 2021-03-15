02e9045d7165225474afc51a84ae8be0576159f5
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-14 20:39:26 -0700

    update project to crrent state

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,8 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ToDoList.Controllers
@@ -10,6 +10,7 @@ namespace ToDoList.Controllers
   public class ItemsController : Controller
   {
     private readonly ToDoListContext _db;
+
     public ItemsController(ToDoListContext db)
     {
       _db = db;
@@ -17,11 +18,11 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.Include(item=>item.Category).ToList();
+      List<Item> model = _db.Items.Include(item => item.Category).ToList();
       return View(model);
     }
 
-    public ActionResult Create() 
+    public ActionResult Create()
     {
       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
@@ -43,7 +44,7 @@ namespace ToDoList.Controllers
 
     public ActionResult Edit(int id)
     {
-      var thisItem = _db.Items.FirstOrDefault(item =>item.ItemId == id);
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View(thisItem);
     }
@@ -58,49 +59,17 @@ namespace ToDoList.Controllers
 
     public ActionResult Delete(int id)
     {
-      var thisItem = _db.Items.FirstOrDefault(item=> item.ItemId == id);
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisItem = _db.Item.FirstOrDefault(item => item.ItemId == id);
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       _db.Items.Remove(thisItem);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-
-    // [HttpGet("/categories/{categoryId}/items/new")]
-    // public ActionResult New(int categoryId)
-    // {
-    //   Category category = Category.Find(categoryId);
-    //   return View(category);
-    // }
-
-    // [HttpPost("/items/delete")]
-    // public ActionResult DeleteAll()
-    // {
-    //   Item.ClearAll();
-    //   return View();
-    // }
-
-    // [HttpGet("/items/{id}")]
-    // public ActionResult Show(int id)
-    // {
-    //   Item foundItem = Item.Find(id);
-    //   return View(foundItem);
-    // }
-
-    // [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    // public ActionResult Show(int categoryId, int itemId)
-    // {
-    //   Item item = Item.Find(itemId);
-    //   Category category = Category.Find(categoryId);
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   model.Add("item", item);
-    //   model.Add("category", category);
-    //   return View(model);
-    // }
   }
 }
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -5,9 +5,11 @@ namespace ToDoList.Models
   public class ToDoListContext : DbContext
   {
     public virtual DbSet<Category> Categories { get; set; }
-    public DbSet<Item> Items { get; set; } //represents Items table
-    public ToDoListContext(DbContextOptions options) : base(options) {}
-    protected overide void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public DbSet<Item> Items { get; set; }
+
+    public ToDoListContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseLazyLoadingProxies();
     }
