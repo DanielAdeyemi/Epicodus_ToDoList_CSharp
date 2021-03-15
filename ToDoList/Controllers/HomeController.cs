61e24abf5d37ee7346addf18dee02d326ad0342e
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-14 21:05:20 -0700

    update project

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -4,16 +4,12 @@ namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
+
     [HttpGet("/")]
     public ActionResult Index()
     {
       return View();
     }
 
-    [HttpGet("/favorite_photos")]
-    public ActionResult FavoritePhotos()
-    {
-      return View();
-    }
   }
 }
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -1,17 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using ToDoList.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore.Design;
 
 namespace ToDoList
 {
@@ -30,6 +23,7 @@ namespace ToDoList
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+
       services.AddEntityFrameworkMySql()
         .AddDbContext<ToDoListContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
@@ -53,6 +47,4 @@ namespace ToDoList
       });
     }
   }
-
-
 }
