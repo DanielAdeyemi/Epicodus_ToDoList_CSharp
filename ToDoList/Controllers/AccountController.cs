fc025ac0d5a0abd228d8a77b2f8e9c65058d5d89
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-22 11:05:34 -0700

    return post route for log in

--- ToDoList/Controllers/AccountController.cs
+++ ToDoList/Controllers/AccountController.cs
@@ -48,6 +48,19 @@ namespace ToDoList.Controllers
     {
       return View();
     }
-    
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
