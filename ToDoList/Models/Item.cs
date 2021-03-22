a976f9c3138502839df8c131064bb7ae97cf4b5e
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-22 11:25:01 -0700

    update Item model

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -12,6 +12,7 @@ namespace ToDoList.Models
     public int ItemId { get; set; }
     public string Description { get; set; }
     public bool Completed { get; set; }
+    public virtual ApplicationUser User { get; set; }
 
     public virtual ICollection<CategoryItem> JoinEntities { get; }
   }
